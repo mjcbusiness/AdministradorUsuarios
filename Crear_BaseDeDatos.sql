be3b418d1b51646fc4462ce8baee4a2eda22d222
mjcbusiness <mjcbusiness28@gmail.com>
2026-02-01 17:08:03 -0300

    Actualización de tabla Usuario, se agregó el campo Eliminado para poder hacer un borrado logico de los datos

--- Crear_BaseDeDatos.sql
+++ Crear_BaseDeDatos.sql
@@ -9,6 +9,7 @@ CREATE TABLE Usuarios (
 	Documento NVARCHAR(50) NOT NULL,
 	Email NVARCHAR(200) NOT NULL UNIQUE,
 	Rol NVARCHAR(20) NOT NULL,
+	Eliminado BIT NOT NULL DEFAULT 0
 );
 -- Agregar restricciones
 --reestricción para el campo Rol	
@@ -19,7 +20,7 @@ ALTER TABLE Usuarios
 ADD CONSTRAINT UQ_Usuarios_Email UNIQUE (Email);
 
 -- Insertar datos de ejemplo
-INSERT INTO dbo.Usuarios (Nombre, Apellido, Documento, Email, Rol)
+INSERT INTO dbo.Usuarios (Nombre, Apellido, Documento, Email, Rol,Eliminado)
     VALUES
-    ('Maxi', 'Cari', '12345678', 'maxi.admin@mail.com', 'Administrador'),
-    ('Juan', 'Pérez', '87654321', 'juan.user@mail.com', 'Usuario');
+    ('Maxi', 'Cari', '12345678', 'maxi.admin@mail.com', 'Administrador',0),
+    ('Juan', 'Pérez', '87654321', 'juan.user@mail.com', 'Usuario',0);
