2ad63ea9f262de5c7d5895e15d7e9b6237ee9820
mjcbusiness <mjcbusiness28@gmail.com>
2026-02-01 17:04:47 -0300

    se creó la infraestructura: interfaz e implementación de usuariorepository

---
+++ Infrastructure/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using AdministradorUsuarios.Domain;
+
+namespace AdministradorUsuarios.Infrastructure
+{
+    public interface IUsuarioRepository
+    {
+        Task<IEnumerable<Usuario>> ObtenerUsuariosAsync();
+        Task<IEnumerable<Usuario>> ObtenerUsuariosPorRolAsync(string rol);
+        Task<Usuario?> ObtenerUsuarioPorIdAsync(int id);   
+        Task<int> CrearAsync(Usuario usuario);
+        Task ActualizarAsync (Usuario usuario);
+        Task EliminarAsync(int id);
+        Task<bool> ExisteEmailAsync(string email,int? id =null);
+
+    }
+}
